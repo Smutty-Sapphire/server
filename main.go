53fd0f23b27764b29e0d91a178235fc100837c89
Vadim Votyakov <nologin700@gmail.com>
2025-07-13 18:39:15 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func httpHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, World! ")
+}
+
+func main() {
+    http.HandleFunc("/", httpHandler)
+    http.ListenAndServe(":8080", nil)
+}
